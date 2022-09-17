b03775213a0cb6928b3aebd65387a85cc78631ba
roa961 <romuloo.otarolaa@gmail.com>
2022-09-17 00:34:45 -0300

    new commit

--- backend/back.py
+++ backend/back.py
@@ -3,9 +3,9 @@ import grpc
 from concurrent import futures
 import proto_web_pb2
 import proto_web_pb2_grpc
-import psycopg2
 import connect
-import time as t
+from time import sleep
+import psycopg2 as ps
 
 
 class Search(proto_web_pb2_grpc.SearchServicer):
@@ -16,23 +16,23 @@ class Search(proto_web_pb2_grpc.SearchServicer):
         item = []
         response=[]
         message = request.message
-        result = f"{message}"
+        result = f'"{message}"'
         cur.execute(query)
         qres = cur.fetchall()
         for i in qres:
-            if item in i[1]:
+            if message in i[1]:
                 item.append(i)
-        for i in item:
+        for j in item:
             result = dict()
-            result['id'] = i[1]
-            result['title'] = i[2]
-            result['description'] = i[3]
-            result['url'] = i[4]
+            result['id'] = j[1]
+            result['title'] = j[2]
+            result['description'] = j[3]
+            result['url'] = j[4]
             #
             response.append(result)
 
         print(proto_web_pb2.Search(search=response))
-        return(proto_web_pb2.Search(search=response))
+        return proto_web_pb2.Search(search=response)
 
 
 def serv():
@@ -43,10 +43,17 @@ def serv():
     server.start()
     server.wait_for_termination()
     
-
+def connectdb():
+    con = ps.connect("dbname=paginas user=postgres password=postgres host=postgresql")
+    return con
 if __name__== "__main__":
-    t.sleep(20)
-    con = connect.conexion()
-    print("conectado")
-    cur = con.cursor()
-    serv()
+    sleep(15)
+    try:
+        con = ps.connect("dbname=paginas user=postgres password=postgres host=postgresql")
+        print(con)
+        cur =con.cursor()
+        print("conectado")
+        #serv()
+    except:
+        print("no se pudo conectar")
+    
