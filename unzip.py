b1b74251af171e52aee2d42305e8e2dcb9d581a8
Rómulo Otárola <42584352+roa961@users.noreply.github.com>
2022-09-18 17:33:17 -0300

    Delete unzip.py

--- unzip.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import gzip
-
-def unziptxt():
-    with gzip.open("user-ct-test-collection-09.txt.gz", 'rb') as in_f:
-        with open('user-ct-test-collection-09.txt','wb')  as out_f:
-            shutil.copyfileobj(in_f, out_f)
-            return True
-
